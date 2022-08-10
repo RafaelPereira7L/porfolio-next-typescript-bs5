32869272b954a74cc0e0a07710c790ce0e081e67
Rafael Pereira <rafaelhpmg@gmail.com>
2022-08-09 22:27:27 -0300

    :adhesive_bandage: Eslint fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,15 +7,24 @@ export default class Document extends NextDocument {
       <Html lang="pt-br">
         <Head>
           <meta charSet="utf-8" />
-          <link rel="stylesheet" href="https://bootswatch.com/5/lux/bootstrap.min.css"/>
-          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.8.1/font/bootstrap-icons.min.css"/>
-          <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"/>
+          <link
+            rel="stylesheet"
+            href="https://bootswatch.com/5/lux/bootstrap.min.css"
+          />
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.8.1/font/bootstrap-icons.min.css"
+          />
+          <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"
+          />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@500&family=JetBrains+Mono&family=Roboto:wght@400;500;700&display=swap"
             rel="stylesheet"
           />
-          <link rel="shortcut icon" href="favicon.jpg" type="image/x-icon"/>
+          <link rel="shortcut icon" href="favicon.jpg" type="image/x-icon" />
         </Head>
         <body>
           <Main />
